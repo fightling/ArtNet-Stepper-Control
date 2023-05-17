65384b339b15d45330cb48d98590286a6c2205f6
Patrick Hoffmann <pat@thats-software.com>
2023-05-17 18:47:24 +0200

    huge refactoring!

--- src/Axis.cpp
+++ src/Axis.cpp
@@ -4,74 +4,90 @@
 class Axis
 {
 public:
-    uint8_t pinPull;
-    uint8_t pinDir;
-    int maxSteps = 512;
-    int stepTarget = 0;
-    int stepCounter = 0;
-    unsigned long sleepTime = 5;
-    int maxSleepTime = 20;
-    pt ptAxis;
+    // compile time configuration
+    const int DATA_INTO_TARGET_FACTOR = 3;
 
-    Axis(int pinPullUp, int pinDirection, int maxStps, int maxSlpTime)
-    { //, int pinBarrier
+    // run time configuration
 
-        pinPull = pinPullUp;
-        pinDir = pinDirection;
-        maxSteps = maxStps;
-        maxSleepTime = maxSlpTime;
-        // lightBarrierPin = pinBarrier;
+    /// pin to trigger stepper motor to MOVE
+    const uint8_t MOVE;
+    /// pin to set direction
+    const uint8_t DIRECTION;
+    /// step range from 0
+    const int MAX_STEPS;
+    /// maximum sleep time??
+    const int MAX_SLEEP_TIME;
+
+    // mutable members
+    int target = 0;
+    int counter = 0;
+    unsigned long sleep_time = 5;
+    pt pt_axis;
+
+    /// constructor
+    /// - `MOVE`: data pin
+    Axis(int MOVE, int dir, int max_steps, int max_sleep_time)
+        : MOVE(MOVE),
+          DIRECTION(dir),
+          MAX_STEPS(max_steps),
+          MAX_SLEEP_TIME(max_sleep_time)
+    {
     }
 
-    int axisThread(struct pt *pt)
+    int doThread(struct pt *pt)
     {
         PT_BEGIN(pt);
-        // Loop forever
         for (;;)
         {
-            int distanceSteps = stepTarget - stepCounter;
-            if (distanceSteps > 0)
+            // set which way to MOVE
+            if (target > counter)
             {
-                digitalWrite(pinDir, HIGH);  // motor direction cw
-                digitalWrite(pinPull, HIGH); // turn the LED on (HIGH is the voltage level)
-                stepCounter++;
+                // motor direction cw
+                digitalWrite(DIRECTION, HIGH);
+                // count steps
+                counter++;
             }
-            else if (distanceSteps < 0)
+            else if (target < counter)
             {
-                digitalWrite(pinDir, LOW);   // motor direction cw
-                digitalWrite(pinPull, HIGH); // turn the LED on (HIGH is the voltage level)
-                stepCounter--;
-            } // if distanceSteps == 0 do nothing
+                // motor direction cw
+                digitalWrite(DIRECTION, LOW);
+                // count steps
+                counter--;
+            }
 
-            PT_SLEEP(pt, sleepTime);
-            digitalWrite(pinPull, LOW); // turn the LED off by making the voltage LOW
-            PT_SLEEP(pt, sleepTime);
+            // turn the LED on
+            digitalWrite(MOVE, HIGH);
+            PT_SLEEP(pt, sleep_time);
+
+            // turn the LED off
+            digitalWrite(MOVE, LOW);
+            PT_SLEEP(pt, sleep_time);
         }
         PT_END(pt);
     }
 
-    void setTarget(const uint8_t data)
+    void parse(const uint8_t *data)
     {
-        stepTarget = int(data) * 3;
+        target = int(data[0]) * DATA_INTO_TARGET_FACTOR;
+        sleep_time = (int)((float)MAX_SLEEP_TIME - round((float)data[1] / 255 * (float)MAX_SLEEP_TIME));
     }
 
-    void setSleepTimeFromDmx(const uint8_t data)
-    {
-        sleepTime = (int)((float)maxSleepTime - round((float)data / 255 * (float)maxSleepTime));
-    }
     void setup()
     {
-        pinMode(pinPull, OUTPUT);
-        pinMode(pinDir, OUTPUT);
-        // pinMode(pinBarrier, INPUT);
-        digitalWrite(pinPull, LOW);
-        digitalWrite(pinDir, LOW);
-        PT_INIT(&ptAxis);
+        // configure pins
+        pinMode(MOVE, OUTPUT);
+        pinMode(DIRECTION, OUTPUT);
+
+        // initialize pins
+        digitalWrite(MOVE, LOW);
+        digitalWrite(DIRECTION, LOW);
+
+        // initialize axis proto thread
+        PT_INIT(&pt_axis);
     }
 
     void loop()
     {
-        PT_SCHEDULE(axisThread(&ptAxis));
-        //}
+        PT_SCHEDULE(doThread(&pt_axis));
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,8 @@
 #include <pt.h>
 #include "protothreads.h"
 #include "./Axis.cpp"
-// this is also valid for other platforms which can use only Ethernet
-// #include <Artnet.h>
 
+// sleep micro seconds
 #define PT_SLEEP_MICROS(pt, delay)                                                                       \
     {                                                                                                    \
         do                                                                                               \
@@ -15,60 +14,91 @@
         } while (false);                                                                                 \
     }
 
-// Ethernet stuff
+#if DEBUG
+#define LOG Serial.println
+#define LOG_INIT() Serial.begin(115200)
+#else
+#define LOG
+#define LOG_INIT()
+#endif
+
+// own IP address?
 const IPAddress ip(192, 168, 0, 201);
+
+// own MAC address?
 uint8_t mac[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB};
 
+// universe
+const uint32_t UNIVERSE = 0; // 0 - 15
+
+// used I/O pins
+const uint8_t AXIS1_PUL_PIN = 40;
+const uint8_t AXIS1_DIR_PIN = 42;
+const uint8_t AXIS2_PUL_PIN = 44;
+const uint8_t AXIS2_DIR_PIN = 46;
+
+// axes movement configuration
+const int AXIS_MAX_STEPS = 1000;
+const int AXIS_SLEEP_TIME = 25;
+
+// global Artnet receiver instance (automatically uses `Ethernet` via `ArtnetEther.h`?)
 ArtnetReceiver artnet;
-uint32_t universe0 = 0; // 0 - 15
-uint8_t AXIS1_PUL_PIN = 40;
-uint8_t AXIS1_DIR_PIN = 42;
-uint8_t AXIS2_PUL_PIN = 44;
-uint8_t AXIS2_DIR_PIN = 46;
+
+// Artnet proto thread
 pt ptArtNet;
-Axis axis1 = Axis(AXIS1_PUL_PIN, AXIS1_DIR_PIN, 1000, 25);
-Axis axis2 = Axis(AXIS2_PUL_PIN, AXIS2_DIR_PIN, 1000, 25);
+
+// axis accessors (based on PINs, steps and timing)
+Axis axis1 = Axis(AXIS1_PUL_PIN, AXIS1_DIR_PIN, AXIS_MAX_STEPS, AXIS_SLEEP_TIME);
+Axis axis2 = Axis(AXIS2_PUL_PIN, AXIS2_DIR_PIN, AXIS_MAX_STEPS, AXIS_SLEEP_TIME);
+
 int artNetThread(struct pt *pt)
 {
     PT_BEGIN(pt);
 
-    // Loop forever
     for (;;)
     {
-        // if distanceSteps == 0 do nothing
-        artnet.parse(); // check if artnet packet has come and execute callback
+        // check if artnet packet has arrived (and eventually execute callback)
+        artnet.parse();
+
+        // have a stroll
         PT_SLEEP(pt, 20);
     }
+
     PT_END(pt);
 }
 
+/// get a message from Artnet (assume it's correct)
 void callback(const uint8_t *data, const uint16_t size)
 {
-    // Serial.println(" Recieved package");
-    axis1.setTarget(data[0]);
-    axis1.setSleepTimeFromDmx(data[1]);
-    axis2.setTarget(data[2]);
-    axis2.setSleepTimeFromDmx(data[3]);
+    LOG("Received package: %02X %02X %02X %02X", data[0], data[1], data[2], data[3]);
+
+    // move axes to coordinates from message
+    axis1.parse(data);
+    axis2.parse(data + 2);
 }
 
 void setup()
 {
-    Serial.begin(115200);
+    LOG_INIT();
 
+    // initialize
     Ethernet.begin(mac, ip);
     artnet.begin();
-    // artnet.subscribe_net(0);     // optionally you can change
-    // artnet.subscribe_subnet(0);  // optionally you can change
 
-    // you can also use pre-defined callbacks
-    artnet.subscribe(universe0, callback);
+    // receive messages from art net to callback
+    artnet.subscribe(UNIVERSE, callback);
+
+    // setup both axis
     axis1.setup();
     axis2.setup();
 }
 
 void loop()
 {
+    // process ArtNet thread
     PT_SCHEDULE(artNetThread(&ptArtNet));
+
+    // process both axes
     axis1.loop();
     axis2.loop();
 }
